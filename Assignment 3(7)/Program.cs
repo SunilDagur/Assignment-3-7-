209583e59d4177f6707f9a3bc5cf57dcab7e6819
Sunil Dagur <sunildagur566@gmail.com>
2023-09-06 22:21:43 +0530

    Add project files.

---
+++ Assignment 3(7)/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class MyClass
+{
+    private int myValue;
+
+    public MyClass(int initialValue)
+    {
+        myValue = initialValue;
+    }
+
+    public void SetValue(int newValue)
+    {
+        myValue = newValue;
+    }
+    public int GetValue()
+    {
+        return myValue;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+       
+        MyClass myObject = new MyClass(10);
+
+       
+        Console.WriteLine("Initial Value: " + myObject.GetValue());
+
+      
+        myObject.SetValue(20);
+
+      
+        Console.WriteLine("Modified Value: " + myObject.GetValue());
+    }
+}
+
